b9e46582769598e8946356b9b50e1ef3c9fcf0d8
mlachman <mlachman@akamai.com>
2026-05-31 21:30:01 +0200

    No need to duplicate krmiva  route

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<KrmelecDashboard />} />
-            <Route path="/krmelec" element={<KrmelecDashboard />} />
             <Route path="/krmiva" element={<Krmiva />} />
           </Route>
         </Routes>
